5d4a6e29bf646fe342d30ea2c64dce6fb04b2858
guneetnarula <guneetnarula@sputznik.com>
2020-07-01 21:32:23 -0400

    enqueue styles

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+define ( 'tlos-theme-version', '1.0' );
+
+/*ENQUEUE STYLES*/
+add_action('wp_enqueue_scripts',function(){
+  wp_enqueue_style('sp-child-css', get_stylesheet_directory_uri().'/assets/css/tlos.css', array('sp-core-style'), '1.0' );
+},99);
+
+?>
