0895fe049ef334bcce5c4af0889f743d288d692c
guneetnarula <guneetnarula@sputznik.com>
2021-08-01 20:32:06 -0400

    single post css update

--- single.php
+++ single.php
@@ -15,12 +15,12 @@
           <?php }
         ?>
 
-        <div class='author-meta'>By <?php
+        <div class='author-and-date'><span class="author-meta">By <?php
 					if ( function_exists('coauthors_posts_links') ) {
 						coauthors_posts_links();
 					}
 					else { the_author(); }
-				?> | Published on <?php the_date('M j, Y');?> </div>
+				?></span> | Published on <?php the_date('M j, Y');?> </div>
 
         <div class="entry-content"><?php the_content(); ?></div>
 
